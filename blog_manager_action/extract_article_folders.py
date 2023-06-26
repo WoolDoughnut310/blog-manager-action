088ee08ee448c1acbb5d5d69a7c32970a7000a0e
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2023-06-26 20:31:46 +0100

    feat: test for v1

--- blog_manager_action/extract_article_folders.py
+++ blog_manager_action/extract_article_folders.py
@@ -11,22 +11,22 @@ def extract_article_folders(files):
     for file in files:
         parts = file.filename.split("/")
         for i in range(-2, -len(parts) - 1, -1):
-            print("/".join(parts[0 : i + 1]))
+            folder = "/".join(parts[0 : i + 1])
             try:
-                dir_contents = repo.get_contents("/".join(parts[0 : i + 1]))
+                dir_contents = repo.get_contents(folder)
 
                 # If `article.md` exists within the directory
                 if any(
                     ["article.md" in file_content.name for file_content in dir_contents]
                 ):
                     if parts[i] not in folders:
-                        folders[parts[i]] = True
+                        folders[folder] = True
 
                     # Create if `article.md` has been created
                     if not (
                         parts[-1] == "article.md" and file.additions == file.changes
                     ):
-                        folders[parts[i]] = False
+                        folders[folder] = False
                     break
             except UnknownObjectException:
                 pass
--- blog_manager_action/main.py
+++ blog_manager_action/main.py
@@ -6,8 +6,7 @@ commit = get_last_commit()
 print("extracting folders")
 folders = extract_article_folders(commit.files)
 print("out folders:", folders)
-# comment out
-folders = {"test-blog/a": False}
+
 for folder in folders.keys():
     print("article folder:", folder)
     publish_article(folder, folders[folder])
